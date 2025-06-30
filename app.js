576d97adc59339c4e6c45c2807633519857808a8
Payal Sharma <roohmeiy@gmail.com>
2025-06-30 11:48:55 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3500;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js demo app!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:3500`);
+});
